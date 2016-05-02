bfe3c58ff11c80adfd6c4ea3c7b12d014f061186
Afton Downey and Bri Popson <aaike001@gmail.com>
2016-05-02 14:59:32 -0700

    add simplifiedcontact form

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -212,7 +212,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'foodblog',
+      'username' => 'foodblog_admin',
+      'password' => 'foodblog',
+      'host' => '127.0.0.1',
+      'port' => '8889',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -244,7 +258,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = '7MVJQQ9cJJcHMP-aHbzzyr6YG6c8Df5_lBVxWJXpVNI';
 
 /**
  * Base URL (optional).
